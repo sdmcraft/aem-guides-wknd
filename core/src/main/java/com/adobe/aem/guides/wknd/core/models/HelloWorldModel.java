ae8dea9884b5786196854bfd8eb8e8a015057c0f
Satya Deep Maheshwari <satyam@adobe.com>
2020-09-21 20:51:37 +0530

    wip

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/HelloWorldModel.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/HelloWorldModel.java
@@ -19,6 +19,7 @@ import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_T
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Default;
@@ -48,6 +49,12 @@ public class HelloWorldModel {
     @SlingObject
     private ResourceResolver resourceResolver;
 
+    @ValueMapValue(injectionStrategy=InjectionStrategy.OPTIONAL)
+    protected String greeting;
+
+    @ValueMapValue(injectionStrategy=InjectionStrategy.OPTIONAL)
+    protected String text;
+
     private String message;
 
     @PostConstruct
@@ -66,5 +73,23 @@ public class HelloWorldModel {
     public String getMessage() {
         return message;
     }
+
+    /***
+     *
+     * @return the value of greeting, if null or blank returns "Hello"
+     */
+    public String getGreeting() {
+        return StringUtils.isNotBlank(this.greeting) ? this.greeting : "Hello";
+    }
+
+    /***
+     *
+     * @return All caps variation of the text value
+     */
+    public String getTextUpperCase() {
+        return StringUtils.isNotBlank(this.text) ? this.text.toUpperCase() : null;
+    }
+
+
 
 }
